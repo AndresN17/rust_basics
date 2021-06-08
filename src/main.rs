7abac55425e86e780af2b9db2b4a330bd41359bc
Andres Noboa <andresnoboa17@hotmail.com>
2021-06-07 20:53:28 -0500

    Added new inclusive ranges

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,21 @@
 fn main() {
     //Declaration of variables
     //This variable is inmutable
-    let nombre ="Andres Noboa";
+    let nombre = "Andres Noboa";
     //Printing a variable with println! function
-    println!("My name is {} and this is my first work in Rust!",nombre);
-    //For make a variable mutable you have to add mut 
+    println!("My name is {} and this is my first work in Rust!", nombre);
+    //For make a variable mutable you have to add mut
     let mut mut_number = 1;
     println!("This is a mutable variable {}", mut_number);
-    mut_number+=1;
+    mut_number += 1;
     println!("This is a mutable variable changing {}", mut_number);
 
+    //Inclusive ranges!
+    for i in 1..3 {
+        println!("i: {}", i);
+    }
+    //New inclusive ranges
+    for i in 1..=3 {
+        println!("i: {}", i);
+    }
 }

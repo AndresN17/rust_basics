4fc14b5a49cb431ef480cd92ecf11e830b073bea
Andres Noboa <andresnoboa17@hotmail.com>
2021-06-07 20:40:51 -0500

    Adding some information and use

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+//Basic features of RUST Language
 fn main() {
-    println!("This is my first work in Rust!");
+    //Declaration of variables
+    //This variable is inmutable
+    let nombre ="Andres Noboa";
+    //Printing a variable with println! function
+    println!("My name is {} and this is my first work in Rust!",nombre);
+    //For make a variable mutable you have to add mut 
+    let mut mut_number = 1;
+    println!("This is a mutable variable {}", mut_number);
+    mut_number+=1;
+    println!("This is a mutable variable changing {}", mut_number);
+
 }

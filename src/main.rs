ae3c615e988ccd7fedd47b3b4a7f70557ab9e9bc
Andres Noboa <andresnoboa17@hotmail.com>
2021-07-06 01:33:49 -0500

    Added new examples for arguments in println function

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,18 @@ fn main() {
     println!("This is a mutable variable {}", mut_number);
     mut_number += 1;
     println!("This is a mutable variable changing {}", mut_number);
+    //Positional arguments can be used in println!
+    println!(
+        "{0} is a servant of the class {1}.",
+        "Artoria Pendragon", "Saber"
+    );
+    //Can be named arguments too
+    println!(
+        "{servant} is a servant of the class {class}",
+        servant = "Artoria Pendragon",
+        class = "Saber"
+    );
+    
 
     //Inclusive ranges!
     for i in 1..3 {
